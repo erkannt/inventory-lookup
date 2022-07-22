6c1ae60a3d32c96bbcc68ed09468a7de9c78e21e
Daniel Haarhoff <daniel@rknt.de>
2022-07-22 08:46:06 +0100

    tidy up

--- src/lookup-item.ts
+++ src/lookup-item.ts
@@ -1,11 +1,11 @@
-type LookupItem = (number: unknown) => Record<string, unknown>
+type LookupItem = (number: unknown) => Record<string, unknown>;
 
 export const lookupItem: LookupItem = () => ({
-	nummer: 383,
-	artikel: "Canon EOS 2000D (SN: 103072022149)",
-	anzahl: 1,
-	verpackung: "Kiste 29",
-	kistenBezeichnung: "Bild Technik",
-	standort: "Kiel",
-	anmerkung: "Seriennr: 103072022149"
-})
+  nummer: 383,
+  artikel: 'Canon EOS 2000D (SN: 103072022149)',
+  anzahl: 1,
+  verpackung: 'Kiste 29',
+  kistenBezeichnung: 'Bild Technik',
+  standort: 'Kiel',
+  anmerkung: 'Seriennr: 103072022149',
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,34 +1,30 @@
 function onScanSuccess(decodedText, decodedResult) {
   fetch('/item/383')
-    .then(
-    function(response) {
-        if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log(
+          'Looks like there was a problem. Status Code: ' + response.status,
+        );
         return;
-        }
+      }
 
-        // Examine the text in the response
-        response.json().then(function(data) {
+      // Examine the text in the response
+      response.json().then(function (data) {
         console.log(data);
-        });
-    }
-    )
-    .catch(function(err) {
-    console.log('Fetch Error :-S', err);
+      });
+    })
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
     });
   console.log(`Code matched = ${decodedText}`, decodedResult);
 }
 
-function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
-  console.warn(`Code scan error = ${error}`);
-}
+function onScanFailure() {}
 
 let html5QrcodeScanner = new Html5QrcodeScanner(
   'reader',
   { fps: 10, qrbox: { width: 250, height: 250 } },
   /* verbose= */ false,
 );
+
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
